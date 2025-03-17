ed450e9f82677b1c0e29e9b44570af1f7076d3d5
Raviteja-Thatipelly <ravitejathatipelli@gmail.com>
2025-03-17 08:46:44 +0530

    second commit

--- main/urls.py
+++ main/urls.py
@@ -3,4 +3,9 @@ from . import views
 
 urlpatterns = [
     path('',views.index,name='app_landing'),
+    path('payment/',views.payment,name='payment'),
+    path('transaction/',views.transaction,name='transaction'),
+    path('wallet/',views.wallet,name='wallet'),
+    path('investment/',views.investment,name='investment'),
+    path('stocksFund/',views.stock_fund,name='stocksFund'),
 ]
--- main/views.py
+++ main/views.py
@@ -4,3 +4,18 @@ from django.shortcuts import render
 
 def index(request):
     return render(request, 'index.html')
+
+def payment(request):
+    return render(request, 'payment.html')
+
+def transaction(request):
+    return render(request, 'transaction.html')
+
+def wallet(request):
+    return render(request, 'wallet.html')
+
+def investment(request):
+    return render(request, 'investment.html')
+
+def stock_fund(request):
+    return render(request, 'stocksFund.html')
